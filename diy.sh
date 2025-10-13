c4b47d48f81636b302a1e6f0dd82b6526bf1fe37
forseti <63159639+forseticn@users.noreply.github.com>
2025-10-13 17:08:55 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -19,23 +19,22 @@ sed -i 's/192.168.1.1/10.0.0.3/g' package/base-files/luci2/bin/config_generate
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
 
 
-# 替换 Clash 核心
-echo "Replacing Clash Meta Core..."
-CLASH_CORE_URL="https://github.com/MetaCubeX/mihomo/releases/download/v1.18.3/mihomo-linux-amd64-compatible.gz"
-mkdir -p ./files/etc/openclash/core/
-wget -qO- $CLASH_CORE_URL | gunzip > ./files/etc/openclash/core/clash_meta
-chmod +x ./files/etc/openclash/core/clash_meta
-
-# passwall 最新版本
+# 替换 Passwall / Passwall2 / MosDNS
 rm -rf feeds/smpackage/luci-app-passwall
 svn export https://github.com/fw876/helloworld/trunk/luci-app-passwall feeds/smpackage/luci-app-passwall
 
-# passwall2 最新版本
 rm -rf feeds/smpackage/luci-app-passwall2
 svn export https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 feeds/smpackage/luci-app-passwall2
 
-# 如果有核心替换，分开放
+rm -rf feeds/smpackage/mosdns
+svn export https://github.com/sbwml/luci-app-mosdns/trunk/mosdns feeds/smpackage/mosdns
+
+# 替换 Clash / MosDNS 二进制
 mkdir -p ./files/etc/openclash/core
-wget ... -O ./files/etc/openclash/core/clash_passwall
-wget ... -O ./files/etc/openclash/core/clash_passwall2
-chmod +x ./files/etc/openclash/core/*
+mkdir -p ./files/usr/bin
+
+wget -qO- https://github.com/MetaCubeX/mihomo/releases/download/v1.18.3/mihomo-linux-amd64-compatible.gz | gunzip > ./files/etc/openclash/core/clash_passwall
+wget -qO- https://github.com/vernesong/OpenClash/releases/download/ClashMeta-v1.18.3/clash_meta.gz | gunzip > ./files/etc/openclash/core/clash_passwall2
+wget -qO- https://github.com/sbwml/mosdns/releases/download/v0.3.1/mosdns_linux_amd64.gz | gunzip > ./files/usr/bin/mosdns
+
+chmod +x ./files/etc/openclash/core/* ./files/usr/bin/mosdns
