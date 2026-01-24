02f8b306cb276547cf10eb99c47b42851dc64e8e
forseti <63159639+forseticn@users.noreply.github.com>
2026-01-24 23:44:06 +0800

    Update diy_p1.sh

--- diy_p1.sh
+++ diy_p1.sh
@@ -1,3 +1,5 @@
-src-git passwall_packages https://github.com/OpenWrt-Passwall/openwrt-passwall-packages.git;main
-src-git passwall https://github.com/OpenWrt-Passwall/openwrt-passwall.git;main
-src-git passwall2 https://github.com/OpenWrt-Passwall/openwrt-passwall2.git;main
+#!/bin/bash
+
+echo 'src-git passwall_packages https://github.com/OpenWrt-Passwall/openwrt-passwall-packages.git;main' >>feeds.conf.default
+echo 'src-git passwall https://github.com/OpenWrt-Passwall/openwrt-passwall.git;main' >>feeds.conf.default
+echo 'src-git passwall2 https://github.com/OpenWrt-Passwall/openwrt-passwall2.git;main' >>feeds.conf.default
