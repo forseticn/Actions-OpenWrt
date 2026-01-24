2d49d00a131b4bc23b0334201e53f5f64588176d
forseti <63159639+forseticn@users.noreply.github.com>
2026-01-24 22:53:08 +0800

    Add Passwall package sources to diy_p1.sh

---
+++ diy_p1.sh
@@ -0,0 +1,3 @@
+src-git passwall_packages https://github.com/OpenWrt-Passwall/openwrt-passwall-packages.git;main
+src-git passwall https://github.com/OpenWrt-Passwall/openwrt-passwall.git;main
+src-git passwall2 https://github.com/OpenWrt-Passwall/openwrt-passwall2.git;main
