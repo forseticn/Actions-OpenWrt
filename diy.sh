ad949a34a782c87c33f0ce5b1aef1b217bc0e4a7
forseti <63159639+forseticn@users.noreply.github.com>
2026-01-24 21:08:42 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,25 +6,19 @@ sed -i 's/192.168.1.1/10.0.0.3/g' package/base-files/files/bin/config_generate
 # 清除登陆密码
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
 
-# 移除旧的 mosdns 和 v2ray-geodata
-rm -rf feeds/packages/net/mosdns feeds/packages/net/v2ray-geodata package/mosdns package/v2ray-geodata
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-
-src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main
-src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main
-src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main
-
-
-# 更新 feeds
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-# 移除 openwrt feeds 自带的核心库
-rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
-git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
-git clone https://github.com/Openwrt-Passwall/openwrt-passwall2 package/passwall2
-
-# 移除 openwrt feeds 过时的luci版本
+# 清理 feeds 里冲突的包
+rm -rf feeds/packages/net/{xray-core,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+rm -rf feeds/packages/net/{mosdns,v2ray-geodata}
 rm -rf feeds/luci/applications/luci-app-passwall
+
+# 安装mosdns
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+# 安装passwall 体系
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall package/passwall-luci
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall2 package/passwall2
