70ac10bf6c5cb98930a4edb70b0a2086b9303e5b
forseti <63159639+forseticn@users.noreply.github.com>
2026-04-13 19:29:37 +0800

    Update diy_p1.sh

--- diy_p1.sh
+++ diy_p1.sh
@@ -1,4 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
-# OpenClash
+echo "== 删除旧版 OpenClash =="
+
+rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf package/*/luci-app-openclash
+
+echo "== 拉取最新版 OpenClash =="
+
 git clone https://github.com/vernesong/OpenClash.git package/OpenClash
