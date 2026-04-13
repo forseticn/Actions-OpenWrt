73c1c4e4089a4cb978e42782b84dd09eee149dac
forseti <63159639+forseticn@users.noreply.github.com>
2026-04-13 22:13:45 +0800

    Update diy_p1.sh

--- diy_p1.sh
+++ diy_p1.sh
@@ -8,8 +8,3 @@ rm -rf package/OpenClash
 # 拉最新OpenClash
 git clone --depth=1 https://github.com/vernesong/OpenClash.git package/OpenClash
 
-# 调整 Docker 到 服务 菜单
-sed -i 's/"admin"/"admin", "services"/g' feeds/luci/applications/luci-app-dockerman/luasrc/controller/*.lua
-sed -i 's/"admin"/"admin", "services"/g; s/admin\//admin\/services\//g' feeds/luci/applications/luci-app-dockerman/luasrc/model/cbi/dockerman/*.lua
-sed -i 's/admin\//admin\/services\//g' feeds/luci/applications/luci-app-dockerman/luasrc/view/dockerman/*.htm
-sed -i 's|admin\\|admin\\/services\\|g' feeds/luci/applications/luci-app-dockerman/luasrc/view/dockerman/container.htm
